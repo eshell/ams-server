4c97db164e296e9eb879fe2f1ff89a895c6b3a52
eric <ericshell2010@gmail.com>
2016-04-18 12:12:57 -0400

    logins

--- ams-moguls.js
+++ ams-moguls.js
@@ -41,6 +41,12 @@ protectedRoutes.use(function(req,res,next){
         res.status(403).send({success:false,msg:'no token provided'});
     }
 });
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+//     next();
+// });
 
 protectedRoutes.all('/test',function(req,res){
     res.send('authorized');
