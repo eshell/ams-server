0d2ec20fe09f06853237c69d224b42616c8dfea5
eric <ericshell2010@gmail.com>
2016-04-21 22:06:01 -0400

    password reset/forgot password

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: 3000,
     jwt:{
         secret:'awesomemusicsite!',
-        expiration: 60*2 //in seconds
+        expiration: 60*60 //in seconds
     },
     mysql:{
         user:'root',
--- routes/account-gateway.js
+++ routes/account-gateway.js
@@ -60,7 +60,57 @@ route.post('/verify',function(req,res){
         res.status(400).send('no code');
     }
 });
+route.post('/forgot-password-update',function(req,res){
+    "use strict";
+    var salt = bcrypt.genSaltSync(10);
 
+    Mogul.update({
+        code: null,
+        password: bcrypt.hashSync(req.body.password, salt)
+    },{
+        where:{
+            code: req.body.code,
+            active:1
+        }
+    }).then(function(mogul){
+        res.status(200).json({message:'Password updated.'});
+    }).catch(function(err){
+        console.log(err);
+        res.send(err).status(400);
+    });
+
+});
+
+route.post('/forgot-password-reset',function(req,res){
+    "use strict";
+        Mogul.count({where:{email:req.body.email, active:1, code: null}}).then(function(count){
+            if(count){
+                var now = new Date();
+                var code = md5(req.body.email+now);
+
+                Mogul.update({
+                    code: code
+                },{
+                    where:{
+                        email:req.body.email,
+                        active:1,
+                        code: null
+                    }
+                }).then(function(mogul){
+                    res.status(200).json({message:'Reset code sent.'});
+                }).catch(function(err){
+                    console.log(err);
+                    res.send(err).status(400);
+                });
+
+            }else{
+                console.log(req.body.email+' hasn\'t been activated, or you have recently been sent reset instructions');
+                res.status(400).send(req.body.email+' hasn\'t been activated, or you have recently been sent reset instructions');
+            }
+        }).catch(function(err){
+            res.status(400).send(err);
+        });
+});
 
 route.post('/register',function(req,res){
     "use strict";
